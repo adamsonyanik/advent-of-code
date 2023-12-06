ad564838c90aa6c5a0fd287c6b30d48ed3ab6e7f
Yanik Adamson <yadamsond@gmail.com>
2023-12-06 09:49:12 +0100

    add string utils default values

--- src/utils/string-parser.ts
+++ src/utils/string-parser.ts
@@ -1,33 +1,89 @@
-declare interface String {
-    lines(): string[];
-    removeWS(): string;
-    reduceWS(): string;
-    numbers(): number[];
-    digits(): number[];
+interface String {
+    /**
+     * splits string
+     * @param separator default is "\n"
+     * @param limit
+     */
+    lines(separator?: string | RegExp, limit?: number | undefined): string[];
+
+    /**
+     * replaces string
+     * @param searchValue default is /\s/g
+     * @param replaceValue default is ""
+     */
+    removeWS(
+        searchValue?: { [Symbol.replace](string: string, replaceValue: string): string },
+        replaceValue?: string
+    ): string;
+
+    /**
+     * replaces string
+     * @param searchValue default is /\s+/g
+     * @param replaceValue default is " "
+     */
+    reduceWS(
+        searchValue?: { [Symbol.replace](string: string, replaceValue: string): string },
+        replaceValue?: string
+    ): string;
+
+    /**
+     * matches string
+     * @param matcher default is /\d+/g
+     */
+    numbers(matcher?: { [Symbol.match](string: string): RegExpMatchArray | null }): number[];
+
+    /**
+     * matches string
+     * @param matcher default is /\d/g
+     */
+    digits(matcher?: { [Symbol.match](string: string): RegExpMatchArray | null }): number[];
+
+    /**
+     * matches string
+     * @param matcher default is /[a-zA-Z]+/g
+     */
+    words(matcher?: { [Symbol.match](string: string): RegExpMatchArray | null }): string[];
+
+    /**
+     * splits string at with ""
+     */
     chars(): string[];
+
     substrFrom(searchStringStart: string): string;
     substrBetweenFirstLast(searchStringStart: string, searchStringEnd?: string): string;
     substrBetweenFirst(searchStringStart: string, searchStringEnd: string, position?: number): string;
 }
 
-String.prototype.lines = function () {
-    return this.split("\n");
+String.prototype.lines = function (separator: string | RegExp = "\n", limit?: number | undefined) {
+    return this.split(separator, limit);
 };
 
-String.prototype.removeWS = function () {
-    return this.replace(/\s/g, "");
+String.prototype.removeWS = function (
+    searchValue: { [Symbol.replace](string: string, replaceValue: string): string } = /\s/g,
+    replaceValue: string = ""
+) {
+    return this.replace(searchValue, replaceValue);
 };
 
-String.prototype.reduceWS = function () {
-    return this.trim().replace(/\s+/g, " ");
+String.prototype.reduceWS = function (
+    searchValue: { [Symbol.replace](string: string, replaceValue: string): string } = /\s+/g,
+    replaceValue: string = " "
+) {
+    return this.trim().replace(searchValue, replaceValue);
 };
 
-String.prototype.numbers = function () {
-    return this.match(/\d+/g)!.map((n) => Number(n));
+String.prototype.numbers = function (matcher: { [Symbol.match](string: string): RegExpMatchArray | null } = /\d+/g) {
+    return this.match(matcher)!.map((n) => Number(n));
 };
 
-String.prototype.digits = function () {
-    return this.match(/\d/g)!.map((n) => Number(n));
+String.prototype.digits = function (matcher: { [Symbol.match](string: string): RegExpMatchArray | null } = /\d/g) {
+    return this.match(matcher)!.map((n) => Number(n));
+};
+
+String.prototype.words = function (
+    matcher: { [Symbol.match](string: string): RegExpMatchArray | null } = /[a-zA-Z]+/g
+) {
+    return this.match(matcher)!.map((s) => s);
 };
 
 String.prototype.chars = function () {
