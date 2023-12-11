0549657011c1d818e0a022b05dc60bf85ddff42c
Yanik Adamson <yadamsond@gmail.com>
2023-12-11 20:15:45 +0100

    fix day12

--- src/test/ts/2023/day11/level1.test.ts
+++ src/test/ts/2023/day11/level1.test.ts
@@ -8,22 +8,14 @@ test("level 1", async () => console.log(run(await readInput(__dirname))));
 function run(_input: string) {
     const input = _input
         .lines()
+        .map((l) => l.chars())
         .map((l) => (!l.includes("#") ? [l, l] : [l]))
         .flat()
-        .map((m) => m.chars())
         .transpose()
-        .map((m) => m.join(""))
         .map((l) => (!l.includes("#") ? [l, l] : [l]))
         .flat();
 
-    const galaxies = input
-        .map((l, y) =>
-            l
-                .chars()
-                .map((c, x) => ({ c, x, y }))
-                .filter((c) => c.c == "#")
-        )
-        .flat();
+    const galaxies = input.map((l, y) => l.map((c, x) => ({ c, x, y })).filter((c) => c.c == "#")).flat();
 
     let dist = 0;
     for (let i = 0; i < galaxies.length; i++)
