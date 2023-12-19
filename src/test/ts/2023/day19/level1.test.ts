3e945f2efa382465eeb4ab4457a6a06785807e94
Yanik Adamson <yadamsond@gmail.com>
2023-12-19 08:34:42 +0100

    fix day19

--- src/test/ts/2023/day19/level1.test.ts
+++ src/test/ts/2023/day19/level1.test.ts
@@ -17,15 +17,12 @@ function run(_input: string) {
                     .substring(id.length + 1, l.length)
                     .split(",")
                     .slice(0, -1)
-                    .map((ins) => {
-                        const i = ins.split(/[><]/);
-                        return {
-                            part: ins.split(/[><]/)[0],
-                            gt: ins.includes(">"),
-                            n: Number(ins.split(/[><]/)[1].split(":")[0]),
-                            target: ins.split(":")[1]
-                        };
-                    }),
+                    .map((ins) => ({
+                        part: ins.split(/[><]/)[0],
+                        gt: ins.includes(">"),
+                        n: Number(ins.split(/[><]/)[1].split(":")[0]),
+                        target: ins.split(":")[1]
+                    })),
                 else: l
                     .split(",")
                     .at(-1)!
@@ -49,18 +46,16 @@ function run(_input: string) {
 
     for (const parts of partsCollection) {
         let stage = "in";
-        while (stage != "A" && stage != "R") {
-            let hasComped = false;
+        stageLoop: while (stage != "A" && stage != "R") {
             const comp = ins.get(stage)!;
             for (const c of comp.ins) {
                 const pn = parts.get(c.part)!.n;
                 if ((c.gt && pn > c.n) || (!c.gt && pn < c.n)) {
                     stage = c.target;
-                    hasComped = true;
-                    break;
+                    continue stageLoop;
                 }
             }
-            if (!hasComped) stage = comp.else;
+            stage = comp.else;
         }
         if (stage == "A") value += [...parts.values()].map((p) => p.n).sum();
     }
