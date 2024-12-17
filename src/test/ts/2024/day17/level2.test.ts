c8d76c55655263bdf64074e4ace5012e94bf5dad
Yanik Adamson <yanik.adamson@gmail.com>
2024-12-18 00:35:03 +0100

    add day17 part2

--- src/test/ts/2024/day17/level2.test.ts
+++ src/test/ts/2024/day17/level2.test.ts
@@ -6,30 +6,58 @@ test("level 2", async () => console.log(run(await readInput(__dirname), false)))
 
 function run(_input: string, _isExample: boolean) {
     const p = _isExample ? "513437217" : "2413751503425530";
-    let reg = 33024962;
-    let out = "";
 
-    let i = 33024962;
-    const max = 0;
+    function t(i: number, value: number[]): number[] | false {
+        if (i >= p.length) return [...value];
+        nextZ: for (let z = 0; z < 8; z++) {
+            const nValue = [...value];
 
-    // eslint-disable-next-line no-constant-condition
-    next: while (i < 0b10000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000 && out != p) {
-        reg = i;
-        out = "";
+            // rShift = z ^ 0b011
+            // r3 = Number(p[i]) ^ z ^ 0b110
 
-        i++;
-        if (i % 100000000 == 0)
-            console.log(i + "/" + 0b10000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000);
+            const n = z.toString(2).padStart(3, "0").digits().reverse();
+            for (const bI of [0, 1, 2])
+                if (nValue[i * 3 + bI] != -1 && nValue[i * 3 + bI] != n[bI]) continue nextZ;
+                else nValue[i * 3 + bI] = n[bI];
 
-        while (reg != 0) {
-            out += ((reg >> ((reg & 0b111) ^ 0b011)) ^ reg ^ 0b110) & 0b111;
-            if (!p.startsWith(out)) continue next;
-            reg = reg >> 3;
+            const rightShift = z ^ 0b011;
+            const o = Number(p[i]) ^ z ^ 0b110;
+            const other = o.toString(2).padStart(3, "0").digits().reverse();
+
+            for (const bI of [0, 1, 2])
+                if (nValue[i * 3 + bI + rightShift] != -1 && nValue[i * 3 + bI + rightShift] != other[bI])
+                    continue nextZ;
+                else nValue[i * 3 + bI + rightShift] = other[bI];
+
+            const tr = t(i + 1, nValue);
+            if (tr) return tr;
         }
-        if (out == p) return i - 1;
+        return false;
     }
 
-    console.log(i);
+    const result = t(0, new Array(128).fill(-1));
 
-    return i;
+    if (result)
+        return parseInt(
+            result
+                .reverse()
+                .map((n) => (n == -1 ? 0 : n))
+                .join(""),
+            2
+        );
 }
+
+/*
+2 4 regs[1] = mod(regs[0], 8);
+1 3 regs[1] = regs[1] ^ 3;
+7 5 regs[2] = Math.floor(regs[0] / Math.pow(2, regs[1]));
+1 5 regs[1] = regs[1] ^ 5;
+0 3 regs[0] = Math.floor(regs[0] / 8);
+4 2 regs[1] = regs[1] ^ regs[2];
+5 5 out.push(mod(regs[1], 8));
+3 0 if (regs[0] == 0) break;
+
+out.push(mod(mod(reg, 8) ^ 3 ^ 5 ^ Math.floor(reg / Math.pow(2, mod(reg, 8) ^ 3)), 8));
+reg = Math.floor(reg / 8);
+if (reg == 0) break;
+ */
